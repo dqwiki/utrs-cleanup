1964869854000666067d26ccb11db4d43cb33008
Amanda <deltaquadwiki@gmail.com>
2019-06-05 19:35:49 -0400

    try split

--- globalfunc.py
+++ globalfunc.py
@@ -51,7 +51,8 @@ def processMembers():
 		try:
 			utrsIDs = text.split("{{UTRS-unblock-user|")
 			for item in utrsIDs:
-				if "UTRSBot" not in item or "\|closed" in item:continue
+				if "UTRSBot" not in item:continue
+				if "closed}}" in item:continue
 				print item
 				utrsID = item.split("|")[0]
 				print utrsID
