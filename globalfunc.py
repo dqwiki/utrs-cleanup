284b82a63ec74f94c32d84f714294c27e6002702
Amanda <deltaquadwiki@gmail.com>
2019-06-05 19:31:31 -0400

    debug

--- globalfunc.py
+++ globalfunc.py
@@ -63,7 +63,9 @@ def processMembers():
 						newstring = templateString +"|closed"
 						newstring = "{{UTRS-unblock-user|"+newstring+"}}"
 						text = text.replace("{{UTRS-unblock-user|"+templateString+"}}",newstring,1)
-						page.save(text,"Syncing closed UTRS appeal status manually")
+						print "Old: ","{{UTRS-unblock-user|"+templateString+"}}"
+						print "New: ",newstring
+						#page.save(text,"Syncing closed UTRS appeal status manually")
 						time.sleep(5)
 					else:
 						print "Appeal #",utrsID," is not closed. SKIPPING"
