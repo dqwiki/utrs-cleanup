1914e60d0a60da206753ab7d0da14e7624fae05f
Amanda <deltaquadwiki@gmail.com>
2019-06-05 11:16:12 -0400

    Hold at 50 for trial

--- globalfunc.py
+++ globalfunc.py
@@ -28,7 +28,7 @@ def getCurrentMembers(category):
         	'list': 'categorymembers',
         	'cmtitle': category,
                 'cmnamespace':'3',
-                'cmlimit':'500',
+                'cmlimit':'50',
                 'format':'json',
                 'rawcontinue':'1'
                 }
@@ -66,4 +66,7 @@ def processMembers():
 		except:
 			print "Failed to get page for: ",user
 			continue
+	page = masterwiki.pages["Wikipedia:Bots/Requests for approval/DeltaQuadBot 7"]
+	text = page.text() + "\n*{{BotTrialComplete}} ~~~~"
+	page.save(text,"Notifying BRFA of trial complete")
 processMembers()
